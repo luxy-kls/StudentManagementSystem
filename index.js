2b7b36b8f0231165e5b64042b0f6adbc00158d36
Keyman <sackeykeyman@gmail.com>
2026-07-01 18:49:12 -0700

    Version 7- Refactoring code

--- index.js
+++ index.js
@@ -13,27 +13,27 @@ function saveStudents(){
     fs.writeFileSync("students.json", data);    
 }
 
+// A function that serves as a container to display the information of a student
+function displayStudent(student){
 
+    console.log(`ID: ${student.id}`);
+    console.log(`Name: ${student.name}`);
+    console.log(`Age: ${student.age}`);
+    console.log(`Course: ${student.course}`);
+    console.log(`Level: ${student.level}`);    
+}
 // A function that displays the list of students 
 function displayStudents(){
     for(let i=0; i<students.length; i++){
-    console.log(`ID: ${students[i].id}`);
-    console.log(`Name: ${students[i].name}`);
-    console.log(`Age: ${students[i].age}`);
-    console.log(`Course: ${students[i].course}`);
-    console.log(`Level: ${students[i].level}`);
+        displayStudent(students[i]);
     }
-    
 }
 
 // A funtion that loops through an array to produce the only level 100 students
 function displayLevel100(){
     for(let i=0; i<students.length; i++){
         if(students[i].level==100){
-            console.log(`ID: ${students[i].id}`);
-            console.log(`Name: ${students[i].name}`);
-            console.log(`Age: ${students[i].age}`);
-            console.log(`Course: ${students[i].course}`);      
+            displayStudent(students[i]);
         }
     }
 }
@@ -46,39 +46,23 @@ function countStudents(){
 
 //A function that allows one to search for a student by ID
 function searchStudentsId(id){
-    for(let i=0; i<students.length; i++){
-        let found = false;
-        if(students[i].id===id){
-            found = true;
+    const student = students.find(student=>student.id === id);
+        if(student){
             console.log("Student Found")
-    console.log(`ID: ${students[i].id}`);
-    console.log(`Name: ${students[i].name}`);
-    console.log(`Age: ${students[i].age}`);
-    console.log(`Course: ${students[i].course}`);
-    console.log(`Level: ${students[i].level}`);
-                break
-        }
-     return found;
-    }
-          if(!found){
+            displayStudent(student);
+        } else{
             console.log("Student Not Found");
         }
-}
-
+    }
 //searchStudentsId(10224)
 
 //A function that allows a student to be searched by Name
 function searchStudentsName(name){
-    for(let i=0; i<students.length; i++){
-        let found = false;
-        if(students[i].name===name){
-            found = true;
+    const student = students.find(student => student.name === name);
+        if(student){
             console.log("Student Found")
-            displayStudents(students[i]);
-                break;
-        }return found;
-    }
-          if(!found){
+            displayStudent(student);
+        } else {
             console.log("Student Not Found");
         }
 }
@@ -105,15 +89,12 @@ function searchStudentsName(name){
 
 // A function that deletes a students from the array
  function deleteStudent(id){
-    let found = false;
-    for(let i=0; i<students.length; i++){
-        if(id===students[i].id){
-            found = true;
-            students.splice(i, 1);
+    const index = students.findIndex(student => student.id === id);
+        if(index !== -1){
+            students.splice(index, 1);
             console.log("Student Deleted Successfully");
             saveStudents();
-        }
-    } if(!found){
+        }else{
         console.log("Student Not Found");
     }
     return students.length;
@@ -121,31 +102,18 @@ function searchStudentsName(name){
 
  // A function that modifies information of a student
  function updateStudent(id, name, age, course, level){
-    let found= false;
-    for(let i=0; i<students.length; i++){
-        if(id===students[i].id){
-            found = true;
-            students[i].age= age;
-            students[i].course= course;
-            students[i].level= level;
-            students[i].name= name;
+    const student = students.find(student => student.id ===id);
+
+        if(student){
+            student.age= age;
+            student.course= course;
+            student.level= level;
+            student.name= name;
             
             saveStudents();
             console.log("Student Information Updated Successfully");
-            break;
-        }
-    }
-    if(!found){
+        }else{
         console.log("Student Not Found");
     }
     
  }
-
-addStudent(
-    10226,
-    "Michael",
-    20,
-    "Mechanical Engineering",
-    100
-);
-console.log(students)
